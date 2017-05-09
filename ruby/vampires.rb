9baeef6d65dfafdc57973afce03f6a6e7851132b
Richard S. Polidoro <armyofbear136@gmail.com>
2017-05-08 19:43:50 -0500

    release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,23 +2,29 @@ wolves_like_sunshine = true
 wolves_like_garlic = true
 vampires_like_sunshine = false
 vampires_like_garlic = false
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.chomp
-puts "What year were you born?"
-yearborn = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
-if ((2017-yearborn == age) || (garlic=="yes" || insurance=="yes"))
-	puts "probably not a vampire"
-else if ((2017-yearborn != age) || (garlic=="no" || insurance=="no"))
-	puts "probably a vampire"
-else if (2017-yearborn != age && garlic=="yes" && insurance=="yes")
-	puts "probably not a vampire"
-else if (name == "Drake Cula" || name == "Tu Fang")
-	puts "Definitely a vampire"
-else
-	puts "Results inconclusive"
+puts"How many employees?"
+employees = gets.chomp.to_i
+while employees > 0
+	puts "What is your name?"
+	name = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp.to_i
+	puts "What year were you born?"
+	yearborn = gets.chomp.to_i
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic = gets.chomp
+	puts "Would you like to enroll in the company’s health insurance?"
+	insurance = gets.chomp
+	if (name == "Drake Cula" || name == "Tu Fang")
+		puts "Definitely a vampire"
+	elsif ((2017-yearborn == age) || (garlic=="yes" || insurance=="yes"))
+		puts "probably not a vampire"
+	elsif ((2017-yearborn != age) || (garlic=="no" || insurance=="no"))
+		puts "probably a vampire"
+	elsif (2017-yearborn != age && garlic=="yes" && insurance=="yes")
+		puts "probably not a vampire"
+	elsif
+		puts "Results inconclusive"
+	end
+	employees -=1
+end
